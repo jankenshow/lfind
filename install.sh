86c9aeb7739c3ff6e1b350bdbda58ec1240ef0ea
jankenshow <>
2020-09-18 08:28:50 +0900

    update installation script

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 chmod -R 700 ./lfind 
 mv ${SCRIPT_DIR}/lfind ~/.local/bin
 
-echo '' >> ~/.bashrc
-echo "# add local commands to path"
+echo -n -e "\n" >> ~/.bashrc
+echo "# add local commands to path" >> ~/.bashrc
 echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
 
 source $HOME/.bashrc
